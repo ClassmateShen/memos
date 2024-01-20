c54ca3fe4e6e5b8eaf019e3cdcac7bb26e4ee372
叫我沈同学 <97521809+ClassmateShen@users.noreply.github.com>
2024-01-20 20:35:39 +0800

    Update About.tsx

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -8,25 +8,17 @@ const About = () => {
       <MobileHeader />
       <div className="w-full px-4 sm:px-6">
         <div className="w-full shadow flex flex-col justify-start items-start px-4 py-3 rounded-xl bg-white dark:bg-zinc-800 text-black dark:text-gray-300">
-          <a href="https://www.usememos.com" target="_blank">
-            <img className="w-auto h-12" src="https://www.usememos.com/full-logo-landscape.png" alt="memos" />
+          <a href="https://blog.shenkx.com/about" target="_blank">
+            <img className="w-auto h-12" src="https://tool.shenkx.com/favicon.png" alt="memos" />
           </a>
-          <p className="text-base">A privacy-first, lightweight note-taking service. Easily capture and share your great thoughts.</p>
+            <p className="text-base">叫我沈同学 的个人 Memos～</p>
           <div className="mt-1 flex flex-row items-center flex-wrap">
-            <Link underline="always" href="https://www.github.com/usememos/memos" target="_blank">
-              GitHub Repo
+            <Link underline="always" href="https://www.github.com/ClassmateShen" target="_blank">
+              GitHub
             </Link>
             <Icon.Dot className="w-4 h-auto opacity-60" />
-            <Link underline="always" href="https://www.usememos.com/" target="_blank">
-              Offical Website
-            </Link>
-            <Icon.Dot className="w-4 h-auto opacity-60" />
-            <Link underline="always" href="https://www.usememos.com/blog" target="_blank">
+            <Link underline="always" href="https://blog.shenkx.com" target="_blank">
               Blogs
-            </Link>
-            <Icon.Dot className="w-4 h-auto opacity-60" />
-            <Link underline="always" href="https://www.usememos.com/docs" target="_blank">
-              Documents
             </Link>
           </div>
         </div>
